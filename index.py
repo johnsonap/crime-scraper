9b4caadbcc12c3f205cd1e1fba634cb63e87c04b
Andrew Johnson <johnsonap@gmail.com>
2013-07-23 19:59:46 -0500

    added json feed

--- index.py
+++ index.py
@@ -1,8 +1,18 @@
-import os
+import os, simplejson
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    fo = open("foo.txt", "r+")
+    suspects = simplejson.loads(fo.read())
+    st = "<ul>"
+    for suspect in suspects['suspects']:
+        st += '<li><img src="' +suspect['img'] + '">' + suspect['name'] + '</li>'
+    return st + '</ul>'
+    
+@app.route('/json')
+def json():
+    fo = open("foo.txt", "r+")
+    return fo.read(), 200, {'Content-Type': 'text/plain'}
